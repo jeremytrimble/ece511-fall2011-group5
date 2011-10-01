7c23324a4302dd937c2620f793667a8a8ad1da43
41mortimer@gmail.com <41mortimer@gmail.com@11cf6cf4-c5a6-a6d3-083f-141811449fba>
2011-10-01 16:11:32 +0000

    Stub "recognition" implementation (and fixing misplaced constant).

--- src/main.c
+++ src/main.c
@@ -3,8 +3,6 @@
 #define LED1 (BIT0)
 #define LED2 (BIT6)
 
-const unsigned long SEQUENCE[] = { 1000, 1000, 500, 500 };
-
 void main(void) {
   WDTCTL = WDTPW | WDTHOLD;
 }
---
+++ src/recognition.c
@@ -0,0 +1,12 @@
+#include "recognition.h"
+
+#define LED1 (BIT0)
+#define LED2 (BIT6)
+
+const unsigned long SEQUENCE[] = { 1000, 1000, 500, 500 };
+
+void report_knock() {
+  P1OUT |= LED1;
+  for (int i=0; i < 2 * DELTA * MS_FACTOR; i++);
+  P1OUT ^= LED1;
+}
