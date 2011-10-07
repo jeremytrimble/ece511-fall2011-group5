f32039257e07c9d0481e47bfa51f78eb4065f64e
jeremy.trimble@gmail.com <jeremy.trimble@gmail.com@11cf6cf4-c5a6-a6d3-083f-141811449fba>
2011-10-07 16:53:47 +0000

    - Added timing.h and timing.c     - init_clock() -- initializes MCLK and SMCLK at 1 MHz.     - delay_milliseconds() and delay_microseconds()
    - Added backchannel.h and backchannel.c
        - Software-based UART with receive and transmit functionality.
        - Transmitting:
            - bc_tx_byte() -- sends a single byte
            - bc_printf() -- a (limited) printf() implementation
        - Receiving:
            - bc_rx_byte() -- receive a single byte (returns -1 if none available)
            - receiving code is interrupt-driven, runs "in the background"
            - implements a 16-byte ring buffer so that RX can happen while the
              processor is doing other things
    
    - Modified detection_simulator to play nice with software UART's use of the Port1 interrupt.
        - Using delay_milliseconds(), counter values are now reported in milliseconds.
    
    - Modified recognition.
        - Removed MS_FACTOR, since counter values are now reported in milliseconds.
    
    - Added bc_printf() messages to recognition, showing the knock history and fail count.
    
    - Added test/test_delay_and_backchannel.c, which tests the functionality of the
      backchannel uart (RX and TX) and the delay_*() functions.

--- trunk/src/detection_simulator.c
+++ trunk/src/detection_simulator.c
@@ -2,27 +2,31 @@
 
 #define BUTTON (BIT3)
 
+#include "timing.h"
+
 unsigned long timer = 0;
 
 extern void init_detection()
 {
-  P1IE = BUTTON;
-  P1IFG = 0;
+    // Switch is active-low, so trigger on press, not release.
+    P1IES |= BUTTON;
 }
 
 extern void run_detection()
 {
-  while(1)
-  {
-    timer++;
-  }
-}
+    while (1)
+    {
+        P1IFG &= ~BUTTON;
+        while ( !(P1IFG & BUTTON) )
+        {
+            delay_milliseconds(1);
+            timer++;
+        }
 
-#pragma vector = PORT1_VECTOR
-__interrupt void port1_isr(void)
-{
-  P1IFG &= ~BUTTON;
-  
-  report_knock(timer);
-  timer = 0;
+        report_knock(timer);
+        timer = 0;
+
+        // wait for release (switch is active low)
+        while ( !(P1IN & BUTTON) );
+    }
 }
--- trunk/src/recognition.c
+++ trunk/src/recognition.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
-#define LED1 (BIT0)
-#define LED2 (BIT6)
+#include "backchannel.h"
+
+#define LED1 (BIT0) // Red LED
+#define LED2 (BIT6) // Green LED
 
 #define DELTA 100
-#define MS_FACTOR 110
 
 const unsigned long KEY[] = { 0, 1000, 1000, 500, 500 };
+
+#define KNOCK_HISTORY_LEN (8)
+#define KNOCK_HISTORY_IDX_MASK (0x07)
+unsigned int knockEventHistory[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char historyIdx = 0;
+
+unsigned int failCounter = 0;
 
 unsigned int index = 0;
 
@@ -14,14 +22,17 @@ void accept_knock();
 void reject_knock();
 
 extern void init_recognition() {
-  P1DIR = (LED1 | LED2);
+  P1DIR |= (LED1 | LED2);
   P1OUT &= ~(LED1 | LED2);
 }
 
 extern void report_knock(unsigned long count)
 {
-  if (index == 0 || (count >= (KEY[index] - DELTA) * MS_FACTOR
-                     && count <= (KEY[index] + DELTA) * MS_FACTOR))
+    knockEventHistory[historyIdx] = (unsigned int) count;
+    historyIdx = (historyIdx + 1) & KNOCK_HISTORY_IDX_MASK;
+
+  if (index == 0 || (count >= (KEY[index] - DELTA)
+                     && count <= (KEY[index] + DELTA) ))
   {
     accept_knock();
   }
@@ -42,7 +53,21 @@ void accept_knock()
   else
   {
     P1OUT |= LED1;
+
+    // Succeeded!
+    bc_printf("Success!\n  Knock history: ");
+    {
+        int i;
+        for (i = 0; i < KNOCK_HISTORY_LEN; i++)
+        {
+            bc_printf("%u ",
+                knockEventHistory[ (i + historyIdx) & KNOCK_HISTORY_IDX_MASK]);
+        }
+    }
+    bc_printf("\n");
+
     index = 0;
+    failCounter = 0;
   }
 }
 
@@ -50,5 +75,11 @@ void reject_knock()
 {
   P1OUT &= ~LED2;
   P1OUT |= LED1;
+
+  // Failed!
+  failCounter++;
+
+  bc_printf("Failed (%d times)!\n  INTRUDER ALERT!\n", failCounter);
+
   index = 0;
 }  
