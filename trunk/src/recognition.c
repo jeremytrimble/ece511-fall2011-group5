3d67f4c8a3d993df3cb63434b88bdcba26cf1e2e
jeremy.trimble@gmail.com <jeremy.trimble@gmail.com@11cf6cf4-c5a6-a6d3-083f-141811449fba>
2011-10-26 02:09:45 +0000

    issue 6 - Added scaling for tempo-independence. issue 13 - Don't reset TAR in WDT ISR.

--- trunk/src/recognition.c
+++ trunk/src/recognition.c
@@ -4,14 +4,14 @@
 #define LED2 (BIT6) // Green LED
 #define SOL_CTRL (BIT7) // Solenoid controller
 
-#define DELTA (unsigned long) 1200
+#define DELTA (unsigned long) 450   /* TODO: experiment more with this. */
 #define KEY_LENGTH (unsigned int) 5
 #define RESET_TIME (unsigned int) 375
 
 #define KNOCK_HISTORY_LEN (8)
 #define KNOCK_HISTORY_IDX_MASK (0x07)
 
-const unsigned long KEY[KEY_LENGTH] = { 0, 6000, 6000, 3000, 3000 };
+const unsigned int KEY[KEY_LENGTH] = { 0, 6000, 6000, 3000, 3000 };
 
 unsigned int knockEventHistory[KNOCK_HISTORY_LEN] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char historyIdx = 0;
@@ -23,6 +23,12 @@ unsigned int index = 0;
 bool wait_for_reset = FALSE;
 
 unsigned int timer_count = 0;
+
+/* Fixed-point scale factor in 4.4 format. 
+ * 4 binary digits to the left of the "virtual decimal point".
+ * 4 binary digits to the right of the "virtual decimal point". 
+ */
+unsigned char scale_factor = 0;
 
 void reset();
 void accept_knock();
@@ -50,10 +56,43 @@ extern void report_knock(unsigned long count)
   {
     accept_knock();
   }
-  else if (index > 0 && count >= (KEY[index] - DELTA)
-           && count <= (KEY[index] + DELTA))
+  else if (index > 0)
   {
-    accept_knock();
+      if ( 1 == index )
+      {
+        // Compute scale factor here
+        // scale_factor = KEY[1] / count
+        unsigned long dividend = (unsigned long)count << 4;
+        unsigned long divisor  = KEY[1];
+        scale_factor = 0;
+        while (dividend >= divisor)     // Poor-man's division.
+        {
+            scale_factor++;
+            dividend -= divisor;
+        }
+      }
+
+      // Apply scale factor.
+      unsigned long scaled_key = ( scale_factor * (unsigned long)KEY[index] ) >> 4;
+
+      if ( 
+          ( /* If scaled_key-DELTA would be < 0, don't do comparison */
+           ( scaled_key < DELTA )
+           ||
+           ( (scaled_key >= DELTA) && ( count >= ( scaled_key - DELTA ) ) )
+          )           
+          && /* If scaled_key + delta would cause overflow, don't do comparison. */
+          ( ((unsigned int)1<<15) - scaled_key < DELTA )  
+          ||
+          ( ((unsigned int)1<<15) - scaled_key >= DELTA &&  count <= ( scaled_key + DELTA ) )
+         )
+      {
+        accept_knock();
+      }
+      else
+      {
+          reject_knock();
+      }
   }
   else
   {
@@ -82,7 +121,7 @@ void accept_knock()
                   knockEventHistory[ (i + historyIdx) & KNOCK_HISTORY_IDX_MASK]);
       }
     }
-    bc_printf("\n");
+    bc_printf("\n Scale factor: (%u >> 4)\n", scale_factor);
     
     index = 0;
     failCounter = 0;
@@ -94,7 +133,7 @@ void reject_knock()
   // Failed!
   failCounter++;
   
-  bc_printf("Failed (%d times)!\n  INTRUDER ALERT!  Knock history: \n", failCounter);
+  bc_printf("Failed (%u times)!\n  INTRUDER ALERT!  Knock history: \n", failCounter);
   {
     int i;
     for (i = 0; i < KNOCK_HISTORY_LEN; i++)
@@ -103,7 +142,7 @@ void reject_knock()
                 knockEventHistory[ (i + historyIdx) & KNOCK_HISTORY_IDX_MASK]);
     }
   }
-  bc_printf("\n");
+  bc_printf("\n Scale factor: (%u >> 4)\n", scale_factor);
   
   index = 0;
   wait_for_reset = TRUE;
@@ -132,5 +171,4 @@ __interrupt void watchdog_timer()
     wait_for_reset = FALSE;
   }
   
-  TACTL |= TACLR;
 }
