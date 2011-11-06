0b905b36f5ef05357a5ecb8222a3627d8582febd
mfelali@gmail.com <mfelali@gmail.com@11cf6cf4-c5a6-a6d3-083f-141811449fba>
2011-11-06 06:10:03 +0000

    updated code for programability functionality. 1- two files are added, programming_mode.c and programming_mode.h 2- programming mode is entered by pushing the push button once, and exited by pushing the push button one more time. On the exit of programming mode, the knocking rythm is played back once. 3- Testing is pain in the neck. since I don't have the microphone circuitry set up. But I have a push button and I tested that the pre-recorded pattern has changed once the user programs it. Further testing is needed.

---
+++ src_5/programming_mode.h
@@ -0,0 +1,10 @@
+#ifndef __PROGRAMMING_MODE_H
+#define __PROGRAMMING_MODE_H
+
+#define KEY_LENGTH (unsigned int) 10
+extern unsigned int KEY[KEY_LENGTH];
+extern void start_programming(unsigned long timer);
+extern void init_programming_mode( );
+extern unsigned int index;
+
+#endif
