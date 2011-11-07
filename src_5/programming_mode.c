0fe2db724ba01e5fcc2008a7262a86e6fd53595d
mfelali@gmail.com <mfelali@gmail.com@11cf6cf4-c5a6-a6d3-083f-141811449fba>
2011-11-07 17:49:43 +0000

    fixed a bug with the programmed key length. The programming mode allows the user to enter a rhythm of up to 10 knocks. - fixed a bug with the first knock timer value, (not being zero).

--- src_5/programming_mode.c
+++ src_5/programming_mode.c
@@ -1,7 +1,7 @@
 #include "main.h"
-unsigned int KEY[KEY_LENGTH] = { 0, 6000,  6000, 3000, 3000, 0, 0, 0, 0 };
-unsigned int index = 0;
-
+unsigned int KEY[KEY_LENGTH] = { 0, 6000,  6000, 3000, 3000, 0, 0, 0, 0,0 };
+unsigned int key_length_programmed = 0;
+unsigned int programming_index =1;
 void init_programming_mode( )
 {
   programming_mode =0;
@@ -9,17 +9,17 @@ void init_programming_mode( )
 void start_programming(unsigned long timer)
 {
   
-  KEY[index]=timer;
+  KEY[programming_index]=timer;
   
-  if  (index < KEY_LENGTH)
+  if  (programming_index < KEY_LENGTH)
       {
-        index++;
+        programming_index++;
       }
       else
       {
-        index = 0;
+        programming_index = 0;
       } 
   
-  
+  key_length_programmed = programming_index+2;//first index is zero +1
   
 }
